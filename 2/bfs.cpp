5068a3e162dd8696f35369754c72ddb6766d8bb0
AshwinDEM <ashwindharmavaram@gmail.com>
2024-08-06 10:59:44 +0530

    Updated the code for bfs.cpp to check complete connectivity instead of between 2 nodes

--- 2/bfs.cpp
+++ 2/bfs.cpp
@@ -1,51 +1,56 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-void bfs(unordered_map<int, vector<int>> &graph, int node, vector<int> &visited, vector<int> &ans)
-{
+void bfs(unordered_map<int, vector<int>>& graph, int start, vector<int>& visited) {
     queue<int> q;
-    q.push(node);
-    visited[node] = 1;
-    while (!q.empty())
-    {
-        int curr = q.front();
+    q.push(start);
+    visited[start] = 1;
+
+    while (!q.empty()) {
+        int node = q.front();
         q.pop();
-        ans.push_back(curr);
-        for (auto i : graph[curr])
-        {
-            if (!visited[i])
-            {
-                q.push(i);
-                visited[i] = 1;
+
+        for (int neighbor : graph[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = 1;
+                q.push(neighbor);
             }
         }
     }
 }
 
-int main()
-{
+bool isConnected(unordered_map<int, vector<int>>& graph, int n) {
+    vector<int> visited(n + 1, 0);
+    bfs(graph, 1, visited);
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            return false; // Graph is not connected
+        }
+    }
+    return true; // Graph is connected
+}
+
+int main() {
     int n, m;
     cin >> n >> m;
     unordered_map<int, vector<int>> graph;
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
-    vector<int> visited(n + 1, 0);
-    vector<int> ans;
-    for (int i = 1; i <= n; i++)
-    {
-        if (!visited[i])
-        {
-            bfs(graph, i, visited, ans);
-        }
+
+    if (isConnected(graph, n)) {
+        cout << "The graph is connected." << endl;
+    } else {
+        cout << "The graph is not connected." << endl;
     }
-    for (auto i : ans)
-    {
-        cout << i << " ";
-    }
+
     return 0;
 }
