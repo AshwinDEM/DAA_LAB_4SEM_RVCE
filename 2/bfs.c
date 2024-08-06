c475b4a6b27e9e12206e024042d29f4a5826e185
AshwinDEM <ashwindharmavaram@gmail.com>
2024-08-06 10:46:35 +0530

    Updated the code for bfs.c to check complete connectivity instead of between 2 nodes

--- 2/bfs.c
+++ 2/bfs.c
@@ -1,86 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 
-double benchmark(int arr[], int n, void* func(int arr[], int n))
-{
-    clock_t start, end;
-    double time_spent;
-    start = clock();
-    func(arr, n);
-    end = clock();
-    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    return time_spent;
-}
+void bfs(int g[100][100], int n, int start, int visited[100]) {
+    int queue[100], front = 0, rear = 0;
+    queue[rear++] = start;
+    visited[start] = 1;
 
-void printArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-void randomarray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = rand() % 100;
-    }
-}
-
-void asc(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = i;
-    }
-}
-
-void desc(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = n - i;
-    }
-}
-
-
-int bfs(int g[][100], int n, int s, int t)
-{
-    int visited[n];
-    for (int i = 0; i < n; i++)
-    {
-        visited[i] = 0;
-    }
-
-    memset(visited, 0, sizeof(visited));
-    int queue[n];
-    int front = 0;
-    int rear = 0;
-    queue[rear] = s;
-    rear++;
-    visited[s] = 1;
-    while (front < rear)
-    {
-        int u = queue[front];
-        front++;
-        for (int i = 0; i < n; i++)
-        {
-            if (g[u][i] == 1 && visited[i] == 0)
-            {
-                queue[rear] = i;
-                rear++;
+    while (front < rear) {
+        int u = queue[front++];
+        for (int i = 0; i < n; i++) {
+            if (g[u][i] == 1 && !visited[i]) {
+                queue[rear++] = i;
                 visited[i] = 1;
             }
         }
     }
-    return visited[t];
 }
 
-int main()
-{
-    
+int isConnected(int g[100][100], int n) {
+    int visited[100] = {0};
+    bfs(g, n, 0, visited);
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            return 0; // Graph is not connected
+        }
+    }
+    return 1; // Graph is connected
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int g[100][100];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &g[i][j]);
+        }
+    }
+
+    if (isConnected(g, n)) {
+        printf("The graph is connected.\n");
+    } else {
+        printf("The graph is not connected.\n");
+    }
+
+    return 0;
 }
